c6e1ae5eb83ff8d1951b824a55baa7c5dad9f43b
lotuspar <91263142+lotuspar@users.noreply.github.com>
2023-05-09 07:39:51 +0800

    Basic hook impls

--- Postage/Hooks.cs
+++ Postage/Hooks.cs
@@ -5,17 +5,28 @@ namespace Postage;
 public static class Hooks
 {
 	[UnmanagedCallersOnly]
-	public static int SandboxEngine_Bootstrap_PreInit( nint gameFolder, int isDedicatedServer, int isRetail,
-		int toolsMode, int testMode, int netVersion, int apiVersion )
+	public static int SandboxEngine_Bootstrap_PreInit( nint ptrRoot, int iDedicated, int iRetail,
+		int iTools, int iTest, int iNet, int iApi )
 	{
-		Log.Info( "hello from preinit" );
+		Log.Info( "PreInit hook worked!" );
+
+		var root = Sandbox.Interop.GetString( ptrRoot );
+		var isDedicated = iDedicated == 1;
+		var isRetail = iRetail == 1;
+		var isTools = iTools == 1;
+		var isTest = iTest == 1;
+		var netVersion = iNet;
+		var apiVersion = iApi;
+
+		Log.Info( $"dedicated {isDedicated}, retail {isRetail}, tools {isTools}, test {isTest}" );
+
 		return 1;
 	}
 
 	[UnmanagedCallersOnly]
 	public static int SandboxEngine_Bootstrap_Init()
 	{
-		Log.Info( "hello from init" );
+		Log.Info( "Init hook worked!" );
 		return 1;
 	}
 }
